7a37ea7b0b9db7ce30be6760a571de0a4a049807
PeterGawecki <pgawecki@hotmail.com>
2018-06-13 17:18:00 -0400

    added firebase/re-base and synced messeages. added day 7 hw

--- src/Chat.js
+++ src/Chat.js
@@ -1,46 +1,54 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 import ChatHeader from './ChatHeader'
 import MessageList from './MessageList'
 import MessageForm from './MessageForm'
 
+import base from './base'
+
 class Chat extends Component {
-    constructor(){
-        super()
+  constructor() {
+    super()
 
-        this.state = {
-            messages: [
-            ],
-        }
+    this.state = {
+      messages: [],
     }
+  }
 
-    addMessage = (body) =>{
-        const messages = [...this.state.messages]
-        messages.push({
-            id: Date.now(),
-            user: this.props.user,
-            body: body,
-        })
-        this.setState({messages})
-    }
+  componentWillMount() {
+    base.syncState('general/messages', {
+      context: this,
+      state: 'messages',
+      asArray: true,
+    })
+  }
 
-    render(){
-        return(
-            <div className="Chat" style = {styles.chat}>
-                <ChatHeader />
-                <MessageList messages={this.state.messages} />
-                <MessageForm addMessage={this.addMessage} />
-            </div>
-        )
-    }
+  addMessage = (body) => {
+    const messages = [...this.state.messages]
+    messages.push({
+      id: Date.now(),
+      user: this.props.user,
+      body,
+    })
+
+    this.setState({ messages })
+  }
+
+  render() {
+    return (
+      <div className="Chat" style={styles}>
+        <ChatHeader />
+        <MessageList messages={this.state.messages} />
+        <MessageForm addMessage={this.addMessage} />
+      </div>
+    )
+  }
 }
 
-const styles ={
-    chat: {
-        flex: 1,
-        display: 'flex',
-        flexDirection: 'column',
-      },
+const styles = {
+  flex: 1,
+  display: 'flex',
+  flexDirection: 'column',
 }
 
 export default Chat
