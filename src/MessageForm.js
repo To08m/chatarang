5599f2b25bab5840078e9b6fec62c75c53528227
PeterGawecki <pgawecki@hotmail.com>
2018-06-13 09:07:41 -0400

    Autofocus the input and clear the form after submit

--- src/MessageForm.js
+++ src/MessageForm.js
@@ -7,6 +7,7 @@ class MessageForm extends Component {
     handleSubmit = (ev) => {
         ev.preventDefault()
         this.props.addMessage(this.state.body)
+        this.setState({body: ''})
     }
 
     handleChange = (ev) => {
@@ -20,6 +21,7 @@ class MessageForm extends Component {
                     type="text" 
                     name="body" 
                     placeholder="Type a message..."
+                    autoFocus
                     value = {this.state.body}
                     onChange = {this.handleChange}
                     style = {styles.input}
